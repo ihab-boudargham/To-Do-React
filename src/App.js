f4891c1af151efd64b1b89489fda4b31f69920e2
ihab-boudargham <ihab.boudargham@hotmail.com>
2024-01-03 23:19:42 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-container h-screen w-screen flex flex-col items-center gap-[1vw]">
+          <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="title-cont h-50px bg-black flex items-center justify-center gap-2 mt-5 rounded-xl">
+      <span className="title">tasX</span>
+      <span className="text-white">-</span>
+      <span className="title-text text-white">get things done</span>
+    </div>
+  )
+}
+
+export default Header
